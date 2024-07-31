56fb0d8f68dbb59afaf72045930956b8fac441b0
ChaimaBSlima <chaimabslima@outlook.fr>
2024-07-31 22:04:39 +0100

    Task 13: Cat's Got Your Tongue

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Task 13: Cat's Got Your Tongue
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenates two matrices along a specific axis.
+
+    Parameters:
+    mat1 : numpy.ndarray
+        The first NumPy array for element-wise operations.
+    mat2 : numpy.ndarray
+        The second NumPy array for element-wise operations.
+
+    Returns:
+        The concatenated matrix.
+    """
+    if axis == 1:
+        return (np.column_stack((mat1, mat2)))
+    else:
+        return (np.vstack((mat1, mat2)))
