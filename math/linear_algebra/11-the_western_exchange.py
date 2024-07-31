dd5a56914516414d98da0f679077b83fa4fcb814
ChaimaBSlima <chaimabslima@outlook.fr>
2024-07-31 21:47:13 +0100

    Task 11:  The Western Exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Task 11:  The Western Exchange
+"""
+
+
+def np_transpose(matrix):
+    """
+    Returns the transpose of a given NumPy matrix.
+
+    Parameters:
+        matrix (numpy.ndarray): The NumPy array or matrix.
+
+    Returns:
+        The transposed matrix.
+    """
+    return (matrix.T)
