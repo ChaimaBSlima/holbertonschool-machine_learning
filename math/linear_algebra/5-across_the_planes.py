90bee49eeeed3bd0d238c6930b1e26bc19f3af6e
ChaimaBSlima <chaimabslima@outlook.fr>
2024-07-31 17:35:05 +0100

    Task 5: 5. Across The Planes removing al if conditions

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -19,13 +19,8 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return (None)
     SommedMat = []
-    if len(mat1[0]) == 0:
-        SommedMat.append([])
-        return SommedMat
     for i in range(len(mat1)):
         SommedMat.append([])
-        if len(mat1[0]) == 0:
-            break
         for j in range(len(mat1[0])):
             SommedMat[i].append(mat1[i][j] + mat2[i][j])
     return SommedMat
