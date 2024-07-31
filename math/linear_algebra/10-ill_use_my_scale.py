0aa5a730016aa9726e0c8a1cda4eb4c456a7825e
ChaimaBSlima <chaimabslima@outlook.fr>
2024-07-31 19:55:45 +0100

    Task 10:  I’ll Use My Scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Task 10:  I’ll Use My Scale
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a given NumPy matrix.
+
+    Args:
+        matrix (numpy.ndarray): The NumPy array or matrix.
+
+    Returns:
+        tuple: A tuple representing the shape of the input matrix,
+        with each element indicating the size of the matrix along
+        a corresponding dimension.
+
+    """
+    return (matrix.shape)
