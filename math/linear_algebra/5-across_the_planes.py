59622a31e935deed21cb496dbbc2fe3eaf41264e
ChaimaBSlima <chaimabslima@outlook.fr>
2024-07-31 12:01:09 +0100

    task 5: 5. Fix: empty matrixs return empty matrix

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -16,13 +16,16 @@ def add_matrices2D(mat1, mat2):
         None: if mat1 and mat2 are not the same shape.
         SommedArr: the new matrice with sommed values.
     """
-    if not mat1 or not mat2 or not mat1[0] or not mat2[0]:
-        return
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
-        return(None)
+        return (None)
     SommedMat = []
+    if len(mat1[0]) == 0:
+        SommedMat.append([])
+        return SommedMat
     for i in range(len(mat1)):
         SommedMat.append([])
+        if len(mat1[0]) == 0:
+            break
         for j in range(len(mat1[0])):
             SommedMat[i].append(mat1[i][j] + mat2[i][j])
     return SommedMat
