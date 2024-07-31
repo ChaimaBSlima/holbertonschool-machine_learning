263f689841573a29beff8fdd10d5434e9a20289d
ChaimaBSlima <chaimabslima@outlook.fr>
2024-07-31 12:18:39 +0100

    task 6: 6. Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 6: 6. Howdy Partner
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+     concatenates two arrays.
+
+    Args:
+        arr1: the first array.
+        arr2: the second array.
+
+    Returns:
+        New array with concatenated values.
+    """
+    return (arr1 + arr2)
