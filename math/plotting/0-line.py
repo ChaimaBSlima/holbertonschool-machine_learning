8730262b561e964b398134f6609bd658e62d71f5
ChaimaBS <chaimabslima@outlook.fr>
2024-08-06 14:50:38 +0100

    task 0:0. Line Graph documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""Task 0: 0. Line Graph"""
 
 
 def line():
+    """
+    Generate and display a line plot of the cubes of numbers from 0 to 10.
 
+    Args:
+        None
+
+    Returns:
+        None
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(np.arange(0, 11), y, 'r')
