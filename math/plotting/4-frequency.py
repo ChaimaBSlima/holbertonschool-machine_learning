49bae1b04039c4d3de63d49957cd78d09434694c
ChaimaBS <chaimabslima@outlook.fr>
2024-08-06 17:36:58 +0100

    Task 4: 4. Frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Task 4: 4. Frequency """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Generate and display a histogram of student grades.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.xlim((0, 100))
+    plt.ylim((0, 30))
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title("Project A")
+    plt.show()
