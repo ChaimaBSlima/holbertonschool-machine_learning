01fed0aca0107d73d03ec7216cb4b1f9e6a68d66
ChaimaBS <chaimabslima@outlook.fr>
2024-10-05 18:57:12 +0100

    Task 3: 3. Projection Block

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -8,49 +8,38 @@ def projection_block(A_prev, filters, s=2):
     Builds a projection block as part of a residual network.
     """
     F11, F3, F12 = filters
+    initializer = K.initializers.he_normal()
 
-    initializer = K.initializers.he_normal(seed=None)
-
-    # Conv 1x1
-    my_layer = K.layers.Conv2D(filters=F11,
-                               kernel_size=(1, 1),
-                               strides=(s, s),
-                               padding='same',
-                               kernel_initializer=initializer,
-                               )(A_prev)
-
-    my_layer = K.layers.BatchNormalization(axis=3)(my_layer)
-    my_layer = K.layers.Activation('relu')(my_layer)
-
-    # Conv 3x3
-    my_layer = K.layers.Conv2D(filters=F3,
-                               kernel_size=(3, 3),
-                               padding='same',
-                               kernel_initializer=initializer,
-                               )(my_layer)
-
-    my_layer = K.layers.BatchNormalization(axis=3)(my_layer)
-    my_layer = K.layers.Activation('relu')(my_layer)
-
-    # Conv 1x1
-    my_layer = K.layers.Conv2D(filters=F12,
-                               kernel_size=(1, 1),
-                               padding='same',
-                               kernel_initializer=initializer,
-                               )(my_layer)
-
-    my_layer = K.layers.BatchNormalization(axis=3)(my_layer)
-    short = K.layers.Conv2D(filters=F12,
-                            kernel_size=(1, 1),
-                            strides=(s, s),
-                            padding='same',
+    conv0 = K.layers.Conv2D(
+        F11, (1, 1),
+        strides=s,
+        kernel_initializer=initializer,
+        padding='same',
+        activation='linear')(A_prev)
+    batch0 = K.layers.BatchNormalization()(conv0)
+    activation0 = K.layers.ReLU()(batch0)
+    conv1 = K.layers.Conv2D(F3, (3, 3),
                             kernel_initializer=initializer,
-                            )(A_prev)
-
-    short = K.layers.BatchNormalization(axis=3)(short)
-
-    output = K.layers.Add()([my_layer, short])
-
-    output = K.layers.Activation('relu')(output)
-
-    return output
+                            padding='same',
+                            activation='linear')(activation0)
+    batch1 = K.layers.BatchNormalization()(conv1)
+    activation1 = K.layers.ReLU()(batch1)
+    conv2 = K.layers.Conv2D(
+        F12,
+        (1, 1),
+        kernel_initializer=initializer,
+        padding='same',
+        activation='linear'
+    )(activation1)
+    conv3 = K.layers.Conv2D(
+        F12,
+        (1, 1),
+        strides=s,
+        kernel_initializer=initializer,
+        padding='same',
+        activation='linear'
+    )(A_prev)
+    batch2 = K.layers.BatchNormalization()(conv2)
+    batch3 = K.layers.BatchNormalization()(conv3)
+    add = K.layers.Add()([batch2, batch3])
+    return K.layers.ReLU()(add)
