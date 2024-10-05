a1a91f81dd0150bf3e9da2d94a0dfebb7a2dae59
ChaimaBS <chaimabslima@outlook.fr>
2024-10-05 18:39:39 +0100

    Test my code with necessary documentation corrections

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -6,28 +6,6 @@ from tensorflow import keras as K
 def identity_block(A_prev, filters):
     """
     Builds a projection block as part of a residual network.
-
-    Parameters
-    A_prev : keras.layers.Layer
-        The output from the previous layer, used as the input for the
-        projection block.
-    filters : list of int
-        A list containing the number of filters for each convolutional layer
-        in the block. The filters should be provided in the order
-        [F11, F3, F12], where:
-        - F11: Number of filters for the first 1x1 convolution.
-        - F3: Number of filters for the 3x3 convolution.
-        - F12: Number of filters for the second 1x1 convolution.
-    s : int, optional
-        The stride value for the first and shortcut 1x1 convolutions
-        (default is 2).
-
-    Returns
-    output : keras.layers.Layer
-        The output of the projection block after performing a series
-        of convolutions, batch normalization, and activation operations.
-        The output is combined with the input through a shortcut path with
-        downsampling.
     """
     F11, F3, F12 = filters
     c1 = K.layers.Conv2D(
