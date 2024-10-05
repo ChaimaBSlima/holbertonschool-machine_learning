ffb8b4a842923c12e19ded16158534cf70aeda7b
ChaimaBS <chaimabslima@outlook.fr>
2024-10-05 21:55:43 +0100

    Task 7: 7. DenseNet-121

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -24,27 +24,27 @@ def densenet121(growth_rate=32, compression=1.0):
         A Keras Model instance representing the DenseNet-121 architecture.
 
     """
-    # implement He et. al initialization for the layers weights
-    initializer = K.initializers.he_normal(seed=0)
+
+    initializer = K.initializers.HeNormal(seed=0)
 
     X = K.Input(shape=(224, 224, 3))
     layers = [12, 24, 16]
 
+    # Initial Batch Normalization and ReLU
     my_layer = K.layers.BatchNormalization(axis=-1)(X)
     my_layer = K.layers.ReLU()(my_layer)
 
-    # Conv 7x7 + 2(S)
+    # Convolution 7x7 + Strides 2
     my_layer = K.layers.Conv2D(filters=2 * growth_rate,
                                kernel_size=(7, 7),
                                strides=(2, 2),
                                padding='same',
-                               kernel_initializer=initializer,
-                               )(my_layer)
+                               kernel_initializer=initializer)(my_layer)
 
-    # MaxPool 3x3 + 2(S)
-    my_layer = K.layers.MaxPool2D(pool_size=(3, 3),
-                                  padding='same',
-                                  strides=(2, 2))(my_layer)
+    # MaxPool 3x3 + Strides 2
+    my_layer = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                      padding='same',
+                                      strides=(2, 2))(my_layer)
 
     nb_filters = 2 * growth_rate
 
@@ -64,15 +64,14 @@ def densenet121(growth_rate=32, compression=1.0):
                                            layer)
 
     # Classification layer
-    # Avg pooling layer with kernels of shape 7x7
+    # Average pooling layer with kernels of shape 7x7
     my_layer = K.layers.AveragePooling2D(pool_size=(7, 7),
                                          padding='same')(my_layer)
 
     # Fully connected softmax output layer with 1000 nodes
     my_layer = K.layers.Dense(units=1000,
                               activation='softmax',
-                              kernel_initializer=initializer,
-                              )(my_layer)
+                              kernel_initializer=initializer)(my_layer)
 
     model = K.models.Model(inputs=X, outputs=my_layer)
 
