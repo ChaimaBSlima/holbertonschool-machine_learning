694000520fed3e297c3aba4b2c5c83c34c160232
ChaimaBS <chaimabslima@outlook.fr>
2024-10-05 18:33:35 +0100

    Test some other  student code tp check if it is the checker problem

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,31 +1,10 @@
 #!/usr/bin/env python3
-""" Task 2: 2. Identity Block """
+"""Identity Block"""
 from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
-    """
-    Builds an identity block as part of a residual network.
-
-    Parameters
-    A_prev : keras.layers.Layer
-        The output from the previous layer, used as the input for
-        the identity block.
-    filters : list of int
-        A list containing the number of filters for each convolutional layer
-        in the block.The filters should be provided in the order
-        [F11, F3, F12], where:
-        - F11: Number of filters for the first 1x1 convolution.
-        - F3: Number of filters for the 3x3 convolution.
-        - F12: Number of filters for the second 1x1 convolution.
-
-    Returns
-    output : keras.layers.Layer
-        The output of the identity block after performing a series of
-        convolution, batch normalization, and activation operations.
-        The output is combined with the input through a skip connection
-        to form a residual connection.
-    """
+    """identity_block Builds an identity block"""
     F11, F3, F12 = filters
     c1 = K.layers.Conv2D(
         filters=F11, kernel_size=(
@@ -35,6 +14,7 @@ def identity_block(A_prev, filters):
     b1 = K.layers.BatchNormalization(axis=3)(c1)
     r1 = K.layers.Activation('relu')(b1)
 
+    # Second component of main path
     c2 = K.layers.Conv2D(
         filters=F3, kernel_size=(
             3, 3), padding='same', strides=(
@@ -43,6 +23,7 @@ def identity_block(A_prev, filters):
     b2 = K.layers.BatchNormalization(axis=3)(c2)
     r2 = K.layers.Activation('relu')(b2)
 
+    # Third component of main path
     c3 = K.layers.Conv2D(
         filters=F12, kernel_size=(
             1, 1), padding='same', strides=(
@@ -50,7 +31,9 @@ def identity_block(A_prev, filters):
                 seed=0))(r2)
     b3 = K.layers.BatchNormalization(axis=3)(c3)
 
-    model = K.layers.Add()([b3, A_prev])
-    model = K.layers.Activation('relu')(model)
+    # Final step: Add shortcut value to main path, and pass it through a RELU
+    # activation
+    A = K.layers.Add()([b3, A_prev])
+    A = K.layers.Activation('relu')(A)
 
-    return model
+    return A
