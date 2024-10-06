89af36b4dbbe955e1e299479406f807c47032c86
ChaimaBS <chaimabslima@outlook.fr>
2024-10-06 07:57:55 +0100

    Task 7: 7. DenseNet-121 change to layers.Activation('relu', name='activation')

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -32,7 +32,7 @@ def densenet121(growth_rate=32, compression=1.0):
 
     # Initial Batch Normalization and ReLU
     my_layer = K.layers.BatchNormalization(axis=3)(X)
-    my_layer = K.layers.ReLU()(my_layer)
+    my_layer = K.layers.Activation('relu', name='activation')(my_layer)
 
     # Convolution 7x7 + Strides 2
     my_layer = K.layers.Conv2D(filters=2 * growth_rate,
