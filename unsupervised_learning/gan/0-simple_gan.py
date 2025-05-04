83e4ae6a0df38afa6a6e5e403da8cc4bbc51f4a9
ChaimaBS <chaimabslima@outlook.fr>
2025-05-04 17:45:16 +0100

    0-simple_gan.py

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -1,172 +1,85 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-import random
-import matplotlib.pyplot as plt
 
-WGAN_clip = __import__('1-wgan_clip').WGAN_clip
+class Simple_GAN(keras.Model):
+    def __init__(self, generator, discriminator, latent_generator, real_examples,
+                 batch_size=200, disc_iter=2, learning_rate=.005):
+        super().__init__()
+        self.latent_generator = latent_generator
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
 
+        self.learning_rate = learning_rate
+        self.beta_1 = .5
+        self.beta_2 = .9
 
-## Regulating the seed
+        # define the generator loss and optimizer
+        self.generator.loss = lambda x: tf.keras.losses.MeanSquaredError()(x, tf.ones_like(x))
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+        self.generator.compile(
+            optimizer=self.generator.optimizer,
+            loss=self.generator.loss
+        )
 
-def set_seeds(seed) :
-    random.seed(seed)
-    np.random.seed(seed)
-    tf.random.set_seed(seed)
+        # define the discriminator loss and optimizer
+        self.discriminator.loss = lambda real_pred, fake_pred: (
+            tf.keras.losses.MeanSquaredError()(real_pred, tf.ones_like(real_pred)) +
+            tf.keras.losses.MeanSquaredError()(fake_pred, -tf.ones_like(fake_pred))
+        )
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+        self.discriminator.compile(
+            optimizer=self.discriminator.optimizer,
+            loss=self.discriminator.loss
+        )
 
+    def get_fake_sample(self, size=None, training=False):
+        if size is None:
+            size = self.batch_size
+        z = self.latent_generator(size)
+        return self.generator(z, training=training)
 
-def hash_tensor(tensor) :
-    return np.sum(np.array([hash(x)%2**30 for x in tensor.numpy().flatten()]))%2**30
+    def get_real_sample(self, size=None):
+        if size is None:
+            size = self.batch_size
+        idx = tf.range(tf.shape(self.real_examples)[0])
+        idx = tf.random.shuffle(idx)[:size]
+        return tf.gather(self.real_examples, idx)
 
+    def train_step(self, data):
+        # Train discriminator
+        for _ in range(self.disc_iter):
+            real_batch = self.get_real_sample()
+            fake_batch = self.get_fake_sample(training=True)
+            with tf.GradientTape() as tape:
+                real_pred = self.discriminator(real_batch, training=True)
+                fake_pred = self.discriminator(fake_batch, training=True)
+                d_loss = self.discriminator.loss(real_pred, fake_pred)
+            grads = tape.gradient(d_loss, self.discriminator.trainable_variables)
+            self.discriminator.optimizer.apply_gradients(
+                zip(grads, self.discriminator.trainable_variables)
+            )
 
-def spheric_generator(nb_points, dim) :
-    u=tf.random.normal(shape=(nb_points, dim))
-    return u/tf.reshape(tf.sqrt(tf.reduce_sum(tf.square(u),axis=[1])+10**-8),[nb_points,1])
+        # Train generator
+        with tf.GradientTape() as tape:
+            fake_batch = self.get_fake_sample(training=True)
+            pred = self.discriminator(fake_batch, training=False)
+            g_loss = self.generator.loss(pred)
+        grads = tape.gradient(g_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(
+            zip(grads, self.generator.trainable_variables)
+        )
 
-def normal_generator(nb_points, dim) :
-    return tf.random.normal(shape=(nb_points, dim))
-
-def uniform_generator(nb_points, dim) :
-    return tf.random.uniform(shape=(nb_points, dim))
-
-
-## Building Generator and Discriminator
-
-def fully_connected_GenDiscr(gen_shape, real_examples, latent_type="normal" ) :
-
-    #   Latent generator
-    if latent_type   == "uniform" :
-        latent_generator  =  lambda k : uniform_generator(k,gen_shape[0])
-    elif latent_type == "normal" :
-        latent_generator  =  lambda k : normal_generator(k,gen_shape[0])
-    elif latent_type == "spheric" :
-        latent_generator  =  lambda k : spheric_generator(k,gen_shape[0])
-
-    #   Generator
-    inputs     = keras.Input(shape=( gen_shape[0] , ))
-    hidden     = keras.layers.Dense( gen_shape[1] , activation = 'tanh'    )(inputs)
-    for i in range(2,len(gen_shape)-1) :
-        hidden = keras.layers.Dense( gen_shape[i] , activation = 'tanh'    )(hidden)
-    outputs    = keras.layers.Dense( gen_shape[-1], activation = 'sigmoid' )(hidden)
-    generator  = keras.Model(inputs, outputs, name="generator")
-
-    #   Discriminator
-    inputs        = keras.Input(shape=( gen_shape[-1], ))
-    hidden        = keras.layers.Dense( gen_shape[-2],   activation = 'tanh' )(inputs)
-    for i in range(2,len(gen_shape)-1) :
-        hidden    = keras.layers.Dense( gen_shape[-1*i], activation = 'tanh' )(hidden)
-    outputs       = keras.layers.Dense( 1 ,              activation = 'tanh' )(hidden)
-    discriminator = keras.Model(inputs, outputs, name="discriminator")
-
-    return generator, discriminator, latent_generator
-
-# illustration:
-def illustr_fully_connected_GenDiscr() :
-    generator, discriminator, latent_generator = fully_connected_GenDiscr([1,100,100,2], None)
-    print(generator.summary())
-    print(discriminator.summary())
-
-
-## Training a fully connected GAN (Simple GAN)
-
-def example_fully_connected_GAN(chosen_type ,real_examples, gen_shape, epochs,  batch_size=200, steps_per_epoch=250, latent_type="normal", learning_rate=.005):
-    generator, discriminator, latent_generator = fully_connected_GenDiscr(gen_shape, real_examples, latent_type=latent_type )
-    G=chosen_type( generator , discriminator , latent_generator, real_examples, learning_rate=learning_rate)
-    G.compile()
-    G.fit(real_examples, epochs=epochs, steps_per_epoch=steps_per_epoch, verbose=1)
-    return G
-
-
-## An example of a large real sample
-
-def four_clouds_example(N_real):
-    X   = np.random.randn( N_real)*.05
-    Y   = np.random.randn( N_real)*.05
-    X[:N_real//2]+=.75
-    X[N_real//2:]+=.25
-    Y[N_real//4:N_real//2]+=.25
-    Y[:N_real//4]+=.75
-    Y[N_real//2:3*N_real//4]+=.75
-    Y[3*N_real//4:]+=.25
-    R   = np.minimum(X*X,1)
-    G   = np.minimum(Y*Y,1)
-    B   = np.maximum(1-R-G,0)
-    return tf.convert_to_tensor(np.vstack([X,Y,R,G,B]).T)
-
-
-## Visualize 5D the result of G
-
-def visualize_5D(G, show=True, title=None, filename=None, dpi=200):
-    fig, axes = plt.subplots(2, 3, figsize=(12, 8))
-    for ax in axes.flatten():
-        ax.set_xlim(-.1, 1.1)
-        ax.set_ylim(-.1, 1.1)
-
-    X_tf = G.real_examples
-    X = X_tf.numpy()
-    axes[0, 0].scatter(X[:, 0], X[:, 1], c=X[:, 2:], s=1)
-    axes[0, 0].set_title("real")
-
-    lat = G.latent_generator(10000)
-    Y_tf = G.generator(lat)
-    Y = Y_tf.numpy()
-    axes[0, 1].scatter(Y[:, 0], Y[:, 1], c=Y[:, 2:], s=1)
-    axes[0, 1].set_title("fake = generator(latent sample) ")
-
-    cX = G.discriminator(X_tf).numpy()
-    cY = G.discriminator(Y_tf).numpy()
-    m = min(np.min(cX), np.min(cY))
-    M = max(np.max(cX), np.max(cY))
-
-    axes[1, 0].scatter(X[:, 0], X[:, 1], c=cX, s=1, vmin=m, vmax=M)
-    axes[1, 0].set_title("discriminator(real)")
-
-    Y = G.generator(G.latent_generator(10000)).numpy()
-    axes[1, 1].scatter(Y[:, 0], Y[:, 1], c=cY, s=1, vmin=m, vmax=M)
-    axes[1, 1].set_title("discriminator(fake)")
-
-    for i in range(2):
-        axes[i, 2].set_xlim(-3, 3)
-        axes[i, 2].set_ylim(-3, 3)
-
-    A = np.linspace(-3, 3, 150)
-    B = np.linspace(-3, 3, 150)
-    X, Y = np.meshgrid(A, B)
-    U = tf.convert_to_tensor(np.vstack([X.ravel(), Y.ravel()]).T)
-
-    X = G.discriminator(G.generator(U)).numpy()
-    u = axes[0, 2].pcolormesh(A, B, X[:, 0].reshape([150, 150]), shading='gouraud')
-
-    axes[0, 2].set_title(r"discriminator $\circ$ generator on latent space")
-
-    axes[1, 2].scatter(lat.numpy()[:, 0], lat.numpy()[:, 1], c=cY, s=1)
-    axes[1, 2].set_title(r"discriminator $\circ$ generator on latent sample")
-    # sci = lambda x : "{:.2E}".format(.99*x*(x>0)+1.01*x*(x<0))
-    # cb=fig.colorbar(u, ax=axes[1])
-    ##cb.set_ticks([sci(np.min(X[:,0])), 0, sci(np.max(X[:,0]))])
-    # cb.ax.tick_params(labelsize=5)
-
-    # some more tuning:
-    for a in axes.flatten():
-        a.tick_params(axis='both', which='major', labelsize=5)
-        a.tick_params(axis='both', which='minor', labelsize=5)
-
-    if title:
-        fig.suptitle(title)
-
-    if filename:
-        plt.savefig(filename, dpi=dpi)
-        if show:
-            plt.show()
-        plt.close()
-
-    elif show:
-        plt.show()
-
-## LET'S GO ! 
-
-set_seeds(0)
-G = example_fully_connected_GAN(WGAN_clip ,four_clouds_example(1000), [2,10,10,5], 16, steps_per_epoch=100, learning_rate=.001)
-visualize_5D(G,show=True, title=f"after 16 epochs")
+        return {"discr_loss": d_loss, "gen_loss": g_loss}
