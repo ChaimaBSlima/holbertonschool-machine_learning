e984e38d2f229c6cb3ac2339a7d057f6d2eaf50c
ChaimaBS <chaimabslima@outlook.fr>
2025-05-18 22:09:32 +0100

    4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -2,6 +2,7 @@
 """ Task 4: 4. FastText """
 import gensim
 
+
 def fasttext_model(sentences, vector_size=100, min_count=5,
                    negative=5, window=5, cbow=True,
                    epochs=5, seed=0, workers=1):
@@ -38,13 +39,6 @@ def fasttext_model(sentences, vector_size=100, min_count=5,
         negative=negative,
         seed=seed,
         workers=workers
-    )
-
-    model.build_vocab(sentences)
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
     )
 
     return model
