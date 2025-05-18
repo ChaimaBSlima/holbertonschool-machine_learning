bf119d774ab05048e8b9d4d9c86f0598c3cff98a
ChaimaBS <chaimabslima@outlook.fr>
2025-05-18 22:07:18 +0100

    4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Task 4: 4. FastText """
-from gensim.models import FastText
+import gensim
 
-
-def fasttext_model(sentences, size=100, min_count=5, window=5, negative=5,
-                   cbow=True, iterations=5, seed=0, workers=1):
+def fasttext_model(sentences, vector_size=100, min_count=5,
+                   negative=5, window=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
     """
     Trains a FastText model on the given sentences.
 
