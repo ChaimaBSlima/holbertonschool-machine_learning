325db64d1d016ae539857d4e04117a550709306a
ChaimaBS <chaimabslima@outlook.fr>
2025-06-08 10:34:18 +0100

    Task 0: 0. Dataset

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -18,11 +18,12 @@ class Dataset:
         Class constructor. Loads the TED HRLR Portuguese to English dataset
         and tokenizes it using a custom vocabulary and Transformers tokenizer.
         """
-        self.data_train, self.data_valid = tfds.load(
-            'ted_hrlr_translate/pt_to_en',
-            split=['train', 'validation'],
-            as_supervised=True
-        )
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train',
+                                    as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation',
+                                    as_supervised=True)
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train
         )
@@ -40,10 +41,9 @@ class Dataset:
             tokenizer_en: PreTrainedTokenizerFast for English.
         """
         tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 13)
 
         tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+            (en.numpy() for pt, en in data), target_vocab_size=2 ** 13)
 
         return tokenizer_pt, tokenizer_en
-
