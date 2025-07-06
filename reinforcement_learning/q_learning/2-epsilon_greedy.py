a6ddfb78f50a418eb2dffd5f6552cd3ce3450058
ChaimaBS <chaimabslima@outlook.fr>
2025-07-06 16:38:02 +0100

    q learning

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Task 2: 2. Epsilon Greedy """
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """[Function that uses epsilon-greedy to determine the next action:]
+    Args:
+        Q ([numpy.ndarray]): [array containing the q-table]
+        state ([type]): [current state]
+        epsilon ([type]): [epsilon to use for the calculation]
+    """
+
+    # https://www.youtube.com/watch?v=HGeI30uATws&list=PLZbbT5o_s2xoWNVdDudn51XM8lOuZ_Njv&index=9
+    ε = epsilon
+    # print(Q)
+    # print(Q.shape)
+    # print(Q[state])
+    # print (state, epsilon)
+
+    to_explore = np.random.randint(Q.shape[1])
+    to_exploit = np.argmax(Q[state, :])
+    exploration_rate_threshold = np.random.uniform(0, 1)
+
+    movement = to_explore if ε > exploration_rate_threshold else to_exploit
+
+    return movement
