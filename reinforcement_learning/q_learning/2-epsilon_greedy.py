3bd110850a5fcd50b000ab98a819bf40c21f0c5c
ChaimaBS <chaimabslima@outlook.fr>
2025-07-06 16:44:25 +0100

    q learning

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -4,24 +4,28 @@ import numpy as np
 
 
 def epsilon_greedy(Q, state, epsilon):
-    """[Function that uses epsilon-greedy to determine the next action:]
+    """
+        Selects the next action using the epsilon-greedy policy.
+    
     Args:
-        Q ([numpy.ndarray]): [array containing the q-table]
-        state ([type]): [current state]
-        epsilon ([type]): [epsilon to use for the calculation]
+        Q (numpy.ndarray): Q-table of shape (states, actions)
+        state (int): current state
+        epsilon (float): probability of choosing a random action (exploration)
+    
+    Returns:
+        int: index of the action selected
     """
 
     # https://www.youtube.com/watch?v=HGeI30uATws&list=PLZbbT5o_s2xoWNVdDudn51XM8lOuZ_Njv&index=9
-    ε = epsilon
+    # ε = epsilon
     # print(Q)
     # print(Q.shape)
     # print(Q[state])
     # print (state, epsilon)
 
-    to_explore = np.random.randint(Q.shape[1])
-    to_exploit = np.argmax(Q[state, :])
-    exploration_rate_threshold = np.random.uniform(0, 1)
-
-    movement = to_explore if ε > exploration_rate_threshold else to_exploit
+    if np.random.uniform(0, 1) < epsilon:
+        movement = np.random.randint(Q.shape[1])
+    else:
+        movement = np.argmax(Q[state])
 
     return movement
