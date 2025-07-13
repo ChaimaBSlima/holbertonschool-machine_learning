e8ba5c30aeb57deb9ab38ed4268414e45ae43995
ChaimaBS <chaimabslima@outlook.fr>
2025-07-13 18:25:38 +0100

    0-monte_carlo.py

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,46 +1,34 @@
 #!/usr/bin/env python3
-"""
-Monte Carlo method for policy evaluation.
-"""
+"""First-visit Monte Carlo algorithm"""
+
 
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000,
-                max_steps=100, alpha=0.1, gamma=0.99):
-    """
-    Performs the Monte Carlo algorithm to update the value estimate.
-
-    Args:
-        env: The environment instance.
-        V (numpy.ndarray): Value estimate array of shape (s,).
-        policy (function): Function that takes a state and
-        returns an action.
-        episodes (int): Number of episodes to train over.
-        max_steps (int): Maximum steps per episode.
-        alpha (float): Learning rate.
-        gamma (float): Discount rate.
-
-    Returns:
-        numpy.ndarray: Updated value estimate array.
-    """
-    for _ in range(episodes):
+def monte_carlo(
+    env, V, policy, episodes=5000,
+    max_steps=100, alpha=0.1, gamma=0.99
+):
+    """Estimates V-value with first-visit Monte Carlo algorithm"""
+    for episode in range(episodes):
         state, _ = env.reset()
-        episode = []
-        done = False
-        for _ in range(max_steps):
+        episode_info = []
+        for step in range(max_steps):
             action = policy(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            episode.append((state, action, reward))
-            state = next_state
-            done = terminated or truncated
-            if done:
+            next_state, reward, terminated, truncated, info = env.step(action)
+            episode_info.append((state, action, reward))
+            if terminated or truncated:
                 break
+            state = next_state
 
-        G = 0.0
-        for t in range(len(episode) - 1, -1, -1):
-            state_t, _, reward_t = episode[t]
-            G = gamma * G + reward_t
-            V[state_t] += alpha * (G - V[state_t])
+        G = 0
+        for ep_index in range(len(episode_info) - 1, - 1, - 1):
+            state, action, reward = episode_info[ep_index]
+            G = gamma * G + reward
+            visited_states = [
+                prev_state for prev_state, _, _ in episode_info[:episode]
+            ]
+            if state not in visited_states:
+                V[state] = V[state] + alpha * (G - V[state])
 
     return V
