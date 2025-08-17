41192e5e2f7997b6cd6324b964c8b53c27d3f80d
ChaimaBS <chaimabslima@outlook.fr>
2025-08-17 19:49:46 +0100

    100-pca.py

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -1,30 +1,54 @@
 #!/usr/bin/env python3
-"""Program that performs PCA color augmentation as described
-in the AlexNet paper"""
+"""Performs PCA color augmentation
+(AlexNet style) using TensorFlow."""
 import tensorflow as tf
-import numpy as np
 
 
 def pca_color(image, alphas):
-    """Function that performs PCA color augmentation as described
-    in the AlexNet paper"""
-    img = tf.keras.preprocessing.image.img_to_array(image)
-    orig_img = tf.keras.preprocessing.image.img_to_array(image)
-    img = img / 255.0
-    img_rs = img.reshape(-1, 3)
-    img_centered = img_rs - np.mean(img_rs, axis=0)
-    img_cov = np.cov(img_centered, rowvar=False)
-    eig_vals, eig_vecs = np.linalg.eigh(img_cov)
-    sort_perm = eig_vals[::-1].argsort()
-    eig_vals[::-1].sort()
-    eig_vecs = eig_vecs[:, sort_perm]
-    m1 = np.column_stack((eig_vecs))
-    m2 = np.zeros((3, 1))
-    alpha = np.random.normal(0, 0.1)
-    m2[:, 0] = alphas * eig_vals[:]
-    add_vect = np.matrix(m1) * np.matrix(m2)
-    for idx in range(3):
-        orig_img[..., idx] += add_vect[idx]
-    orig_img = np.clip(orig_img, 0.0, 255.0)
-    orig_img = orig_img.astype(np.uint8)
-    return orig_img
+    """
+    Performs PCA color augmentation (AlexNet style).
+
+    Args:
+        image: A 3D tf.Tensor (H, W, 3) containing the image.
+        alphas: Tuple of length 3 with the perturbation factors.
+
+    Returns:
+        The PCA color-augmented image as a tf.Tensor.
+    """
+    image = tf.cast(image, tf.float32)
+    shape = tf.shape(image)
+
+    # Flatten pixels to (num_pixels, 3)
+    flat_img = tf.reshape(image, [-1, 3])
+
+    # Compute covariance matrix (3x3)
+    mean = tf.reduce_mean(flat_img, axis=0, keepdims=True)
+    centered = flat_img - mean
+    cov = tf.matmul(centered, centered,
+                    transpose_a=True) / tf.cast(
+                        tf.shape(centered)[0] - 1, tf.float32)
+
+    # Eigen decomposition
+    eigvals, eigvecs = tf.linalg.eigh(cov)
+
+    # Sort eigenvalues & eigenvectors descending
+    idx = tf.argsort(eigvals, direction='DESCENDING')
+    eigvals = tf.gather(eigvals, idx)
+    eigvecs = tf.gather(eigvecs, idx, axis=1)
+
+    # Compute perturbation: eigvecs @ (eigvals * alphas)
+    delta = tf.matmul(
+        eigvecs,
+        tf.reshape(eigvals * tf.constant(alphas, dtype=tf.float32), [-1, 1])
+    )
+    delta = tf.reshape(delta, [1, 3])  # shape (1,3)
+
+    # Add delta to all pixels
+    flat_img = flat_img + delta
+
+    # Reshape back to image
+    img_out = tf.reshape(flat_img, shape)
+
+    # Clip to [0,255] and convert back to uint8
+    img_out = tf.clip_by_value(img_out, 0.0, 255.0)
+    return tf.cast(img_out, tf.uint8)
